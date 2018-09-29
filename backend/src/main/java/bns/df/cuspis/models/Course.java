7c7d0b81ab584e49c71885bee82ecdb9dc0af083
Ding Zhu <dzhu2017@gmail.com>
2018-09-29 10:48:00 -0400

    basic structure

---
+++ backend/src/main/java/bns/df/cuspis/models/Course.java
@@ -0,0 +1,9 @@
+package bns.df.cuspis.models;
+
+public class Course {
+    private String id;
+
+    private String courseName;
+    
+    private String code;
+}
---
+++ backend/src/main/java/bns/df/cuspis/repositories/CourseRepository.java
@@ -0,0 +1,7 @@
+package bns.df.cuspis.repositories;
+
+import bns.df.cuspis.models.Course;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, String> {
+}
