d24ed83b7c75a25137154999acba6efc914920bd
Ding Zhu <dzhu2017@gmail.com>
2018-09-28 22:28:29 -0400

    initial commit SpringBoot + MongoDB

---
+++ src/main/java/bns/df/cuspis/CuspisApplication.java
@@ -0,0 +1,12 @@
+package bns.df.cuspis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuspisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuspisApplication.class, args);
+	}
+}
